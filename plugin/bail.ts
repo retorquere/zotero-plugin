0a65b724cc47a1ae38b432071d0954d890f9abd3
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-03-13 09:06:01 +0100

    bailplugin no longer required in webpack 4

--- plugin/bail.ts
+++
@@ -1,7 +0,0 @@
-export default function BailPlugin() {
-  compiler.hooks.done.tap('BailPlugin', stats => {
-    while (stats.compilation.warnings.length) {
-      stats.compilation.errors.push(stats.compilation.warnings.pop())
-    }
-  })
-}

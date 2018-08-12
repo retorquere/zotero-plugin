e5408c9a74a63fe8786b4963a3ec92b8031a3739
Emiliano Heyns <emiliano.heyns@han.nl>
2018-08-12 22:44:52 +0200

    test for asset dups

--- bin/release.ts
+++ bin/release.ts
@@ -85,9 +85,8 @@ async function uploadAsset(release, asset, contentType) {
   if (dryRun) return
 
   const name = path.basename(asset)
-  let assets = await octokit.repos.getAssets({ owner, repo, release_id: release.data.id })
-  assets = assets.data.map(asset => asset.name)
-  if (assets.includes(name)) bail(`failed to upload ${path.basename(asset)} to ${release.data.html_url}: asset exists`);
+  const assets: string[] = (await octokit.repos.getAssets({ owner, repo, release_id: release.data.id })).data.map(asset => asset.name)
+  if (assets.includes(name)) bail(`failed to upload ${path.basename(asset)} to ${release.data.html_url}: asset exists`)
 
   try {
     await octokit.repos.uploadAsset({

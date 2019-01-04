3389e23cd64ed52a8bfa37cd044663ef5a47d329
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-01-04 13:32:42 +0100

    use package name in wrapper loader

--- loader/wrap.ts
+++ loader/wrap.ts
@@ -1,20 +1,25 @@
+import * as path from 'path'
+
+import root from '../root'
+const pkg = { ...require(path.join(root, 'package.json')) }
+
 export = function loader(source) {
   // this.cacheable()
 
   const src = this.resourcePath.substring(process.cwd().length + 1)
 
   const loading = `
-    Zotero.debug('BBT: loading ${src}')
+    Zotero.debug('${pkg.name}: loading ${src}')
   `
   const loaded = `
-    Zotero.debug('BBT: loaded ${src}')
+    Zotero.debug('${pkg.name}: loaded ${src}')
   `
 
   const id = src.replace(/[^a-zA-Z0-9]/g, '_')
   const errvar = '$wrap_loader_catcher_' + id
   const msgvar = '$wrap_loader_message_' + id
   const failed = `
-    var ${msgvar} = 'Error: BBT: load of ${src} failed:' + ${errvar} + '::' + ${errvar}.stack;
+    var ${msgvar} = 'Error: ${pkg.name}: load of ${src} failed:' + ${errvar} + '::' + ${errvar}.stack;
     if (typeof Zotero.logError === 'function') {
       Zotero.logError(${msgvar})
     } else {

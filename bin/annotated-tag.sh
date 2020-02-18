9e8e57a61a199b6c191b8385260801ace18d864f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-02-18 19:19:39 +0100

    remove obsolete bin

--- bin/annotated-tag.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-TAG=$(echo $(git describe --tags $(git rev-list --tags --max-count=1)))
-git tag -a $TAG $TAG -f -m "$(git log `git describe --tags --abbrev=0 HEAD^`..HEAD --oneline)"
-git push && git push --tags

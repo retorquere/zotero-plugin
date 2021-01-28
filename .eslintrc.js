9b1bb5e498043d3ad4efd88814643b51dff8cd20
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-01-28 19:18:20 +0100

    more rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     "extends": [
         'eslint:recommended',
         "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking"
     ],
     "parser": "@typescript-eslint/parser",

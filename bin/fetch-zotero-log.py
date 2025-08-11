170fe18fbc8c719f690b1017e83e0ab2fb641a9e
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-08-11 08:42:19 +0200

    debug log

--- bin/fetch-zotero-log.py
+++ bin/fetch-zotero-log.py
@@ -3,25 +3,58 @@
 import sys, os
 import urllib.request
 from zipfile import ZipFile
+from types import SimpleNamespace
 
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 
-local, host, remote = sys.argv[1].split('-')
-assert host == '0x0', sys.argv[1]
-url = f'https://0x0.com/{remote}.zip'
-log = 'logs/' + local + '.zip'
-print(url, '=>', log)
-logs = os.path.dirname(log)
-if not os.path.exists(logs):
-  os.makedirs(logs)
+def debuglog():
+  local, host, remote = sys.argv[1].split('-')
+  if host != '0x0':
+    print('unexpected debug log host', host)
+    sys.exit(1)
 
-urllib.request.urlretrieve(url, log)
-with ZipFile(log) as f:
-  for name in f.namelist():
-    if not '/' in name: raise ValueError(name)
-    print(name)
-  f.extractall(path=logs)
-os.remove(log)
-print(log)
+  encrypted = ('.') in remote
+  remote = remote.split('.')[0]
+  ext = '.enc' if encrypted else '.zip'
+  url = f'https://0x0.com/{remote}{ext}'
+  return SimpleNamespace(local=local, host=host, remote=remote, encrypted=encrypted, ext=ext, url=url)
+
+def download():
+  debuglog.downloaded = f'logs/{debuglog.local}{debuglog.ext}'
+  print(debuglog.url, '=>', debuglog.downloaded)
+  logs = os.path.dirname(log)
+  if not os.path.exists(logs):
+    os.makedirs(logs)
+  urllib.request.urlretrieve(debuglog.url, debuglog.downloaded)
+
+def decrypt():
+  if not debuglog.encrypted:
+    return
+
+  encrypted = debuglog.downloaded
+  debuglog.downloaded = f'logs/{debuglog.local}.zip'
+
+  with open(sys.argv[2], 'rb') as f:
+    private_key = load_pem_private_key(f.read(), password=None)
+  with open(encrypted, 'rb') as f:
+    encrypted_data = f.read()
+  with open(debuglog.downloaded, 'wb') as f:
+    f.write(private_key.decrypt(encrypted_data, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)))
+
+def show():
+  with ZipFile(debuglod.downloaded) as f:
+    for name in f.namelist():
+      if not '/' in name:
+        print('Unexpected', name, 'in', sys.argv[1])
+        sys.exit(1)
+      print(name)
+    f.extractall(path='logs')
+  os.remove(debuglog.downloaded)
+  print(debuglog.local)
+
+debuglog = debuglog()
+download()
+decrypt()
+show()
--- debug-log.ts
+++ debug-log.ts
@@ -140,7 +140,7 @@ class DebugLogSender {
     const zip = new Uint8Array(UZip.encode(files) as ArrayBuffer)
 
     let blob: Blob
-    let mark = ''
+    let ext = ''
 
     if (pubkey) {
       try {
@@ -153,7 +153,7 @@ class DebugLogSender {
         const publicKey = await subtle.importKey('spki', keyBuffer, { name: 'RSA-OAEP', hash: 'SHA-256' }, true, ['encrypt'])
         const encrypted = await subtle.encrypt({ name: 'RSA-OAEP' }, publicKey, zip)
         blob = new Blob([encrypted], { type: 'application/octet-stream' })
-        mark = 'enc:'
+        ext = '.enc'
       }
       catch (err) {
         Services.prompt.alert(null, `Log encryption for ${plugin} failed`, err.message)
@@ -163,7 +163,7 @@ class DebugLogSender {
     if (!blob) blob = new Blob([zip], { type: 'application/zip' })
 
     const formData = new FormData()
-    formData.append('file', blob, `${key}.zip`)
+    formData.append('file', blob, `${key}${ext || '.zip'}`)
     formData.append('expire', `${7 * 24}`)
 
     try {
@@ -177,7 +177,7 @@ class DebugLogSender {
       const body = await response.text()
       const id = body.match(/https:\/\/0x0.st\/([A-Z0-9]+)\.zip/i)
       if (!id) throw new Error(body)
-      Services.prompt.alert(null, `Debug log ID for ${plugin}`, `${key}-0x0-{mark}${id[1]}`)
+      Services.prompt.alert(null, `Debug log ID for ${plugin}`, `${key}-0x0-${id[1]}{ext}`)
     }
     catch (err) {
       Services.prompt.alert(null, `Could not post debug log for ${plugin}`, err.message)

336c8be4b28e65154b6119395613451650e1a3e8
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-08-12 13:47:03 +0200

    encrypted logs

--- bin/fetch-zotero-log.py
+++ bin/fetch-zotero-log.py
@@ -4,57 +4,101 @@ import sys, os
 import urllib.request
 from zipfile import ZipFile
 from types import SimpleNamespace
+from pathlib import Path
 
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+
+def oops(*args):
+  print(*args)
+  sys.exit(1)
 
 def debuglog():
-  local, host, remote = sys.argv[1].split('-')
-  if host != '0x0':
-    print('unexpected debug log host', host)
-    sys.exit(1)
+  if len(sys.argv) < 2:
+    oops('No log ID')
 
-  encrypted = ('.') in remote
-  remote = remote.split('.')[0]
-  ext = '.enc' if encrypted else '.zip'
-  url = f'https://0x0.com/{remote}{ext}'
-  return SimpleNamespace(local=local, host=host, remote=remote, encrypted=encrypted, ext=ext, url=url)
+  logid = Path(sys.argv[1])
+  tags = logid.suffixes
+  logid = str(logid).rstrip(''.join(tags))
+
+  try:
+    key, host, remote = logid.split('-')
+  except ValueError:
+    oops('Invalid log ID', sys.argv[1])
+
+  if host != '0x0':
+    oops('unexpected debug log host', host)
+
+  encrypted = ('.enc' in tags)
+  references = ('.refs' in tags)
+  cypher_rsa = None
+  if encrypted:
+    if len(len(sys.argv) != 3):
+      oops('no private key provided')
+    if Path(sys.argv[2]).suffix != '.pem':
+      oops('private key must be a .pem')
+
+  url = f'https://0x0.com/{remote}.zip'
+  SimpleNamespace(key=key, host=host, remote=remote, encrypted=encrypted, references=references, url=url),
+debuglog = debuglog()
 
 def download():
-  debuglog.downloaded = f'logs/{debuglog.local}{debuglog.ext}'
-  print(debuglog.url, '=>', debuglog.downloaded)
+  debuglog.zip = f'logs/{debuglog.key}.zip'
+  print(debuglog.url, '=>', debuglog.zip)
   logs = os.path.dirname(log)
   if not os.path.exists(logs):
     os.makedirs(logs)
-  urllib.request.urlretrieve(debuglog.url, debuglog.downloaded)
+  urllib.request.urlretrieve(debuglog.url, debuglog.zip)
 
-def decrypt():
-  if not debuglog.encrypted:
-    return
+def decrypt(encrypted, iv, target):
+  encrypted = encrypted.read()
+  iv = iv.read()
 
-  encrypted = debuglog.downloaded
-  debuglog.downloaded = f'logs/{debuglog.local}.zip'
+  # The last 16 bytes of the encrypted file are the authentication tag for GCM
+  tag = encrypted[-16:]
+  ciphertext = encrypted[:-16]
 
-  with open(sys.argv[2], 'rb') as f:
-    private_key = load_pem_private_key(f.read(), password=None)
-  with open(encrypted, 'rb') as f:
-    encrypted_data = f.read()
-  with open(debuglog.downloaded, 'wb') as f:
-    f.write(private_key.decrypt(encrypted_data, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)))
+  cipher_aes = AES.new(symmetric_key, AES.MODE_GCM, nonce=iv)
+  decrypted = cipher_aes.decrypt_and_verify(ciphertext, tag)
+  with open(target, 'wb') as f:
+    f.write(decrypted)
 
-def show():
-  with ZipFile(debuglod.downloaded) as f:
-    for name in f.namelist():
-      if not '/' in name:
-        print('Unexpected', name, 'in', sys.argv[1])
-        sys.exit(1)
-      print(name)
-    f.extractall(path='logs')
-  os.remove(debuglog.downloaded)
-  print(debuglog.local)
+def unpack():
+  symmetric_key = None
 
-debuglog = debuglog()
+  with ZipFile(debuglog.zip) as zip:
+    files = zip.namelist()
+
+    symmetric_key = [f for f in files if Path(f).suffix == '.key']
+    match len(private_key):
+      case 0:
+        if debuglog.encrypted:
+          oops('No key found in', debuglog.key)
+
+      case 1:
+        with open(sys.argv[2], 'rb') as f:
+          private_key = RSA.import_key(f.read())
+          cipher_rsa = PKCS1_OAEP.new(private_key)
+        with zip.open(symmetric_key[0]) as f:
+          symmetric_key = cipher_rsa.decrypt(f.read())
+
+      case _:
+        oops('Multiple keys found in', debuglog.key)
+
+    for name in files:
+      match Path(name).suffix:
+        case '.key' | '.iv'
+          pass
+        case '.enc'
+          print(Path('logs') / name.with_suffix('.zip'), '(encrypted)')
+          iv = next((f for f in filename_list if f == Path(name).with_suffix('.iv')), None)
+
+          with zip.open(name) as f_data, open(iv) as f_iv:
+            decrypt(f_data, f_iv, str(Path('logs') / name))
+
+        case _:
+          print(Path('logs') / name)
+          f.extract(name, path='logs')
+
 download()
 decrypt()
-show()
--- debug-log.ts
+++ debug-log.ts
@@ -1,10 +1,79 @@
 /* eslint-disable no-magic-numbers */
 
+Components.utils.importGlobalProperties(['FormData'])
+
 type ZoteroPane = {
   getSelectedItems: () => any[]
 }
 
 const pkg = require('./package.json')
+
+import * as UZip from 'uzip'
+
+export class Bundler {
+  public key: string
+
+  #refs = false
+
+  private IV_LENGTH = 12
+  #symmetric: CryptoKey
+  #pubkey: string
+
+  #crypto: Crypto
+  #subtle: SubtleCrypto
+  #files: Record<string, Uint8Array> = {}
+
+  #encoder = new TextEncoder
+
+  constructor(pubkey: string) {
+    this.key = Zotero.Utilities.generateObjectKey()
+    this.#pubkey = pubkey
+    this.#crypto = Zotero.getMainWindow().crypto
+    this.#subtle = this.#crypto.subtle
+  }
+
+  async add(path: string, data: string, refs=false): Promise<void> {
+    this.#refs = this.#refs || refs
+
+    const encoded = this.#encoder.encode(data)
+
+    if (this.#pubkey) {
+      if (!this.#symmetric) {
+        this.#symmetric = await this.#subtle.generateKey({ name: 'AES-GCM', length: 256 }, true, ['encrypt', 'decrypt'])
+
+        const base64Pem = this.#pubkey
+          .replace('-----BEGIN PUBLIC KEY-----', '')
+          .replace('-----END PUBLIC KEY-----', '')
+          .replace(/\s/g, '')
+        const keyBuffer = Uint8Array.from(atob(base64Pem), c => c.charCodeAt(0)).buffer
+        const publicKey = await this.#subtle.importKey('spki', keyBuffer, { name: 'RSA-OAEP', hash: 'SHA-256' }, true, ['encrypt'])
+        const exportedKey = await this.#subtle.exportKey('raw', this.#symmetric)
+
+        this.#files[`${this.key}/${this.key}.key`] = new Uint8Array(await this.#subtle.encrypt({ name: 'RSA-OAEP' }, publicKey, exportedKey))
+      }
+
+      const iv = this.#crypto.getRandomValues(new Uint8Array(this.IV_LENGTH))
+      const encryptedData = await this.#subtle.encrypt({ name: 'AES-GCM', iv: iv }, this.#symmetric, encoded)
+      this.#files[`${this.key}/${path}.iv`] = iv
+      this.#files[`${this.key}/${path}.enc`] = new Uint8Array(encryptedData)
+    }
+    else {
+      this.#files[`${this.key}/${path}`] = encoded
+    }
+  }
+
+  public get zip(): ArrayBuffer {
+    return UZip.encode(this.#files) as ArrayBuffer
+  }
+
+  public get name(): string {
+    return `${this.key}.zip`
+  }
+
+  public id(remote: string): string {
+    return `${this.key}-${remote}${this.#refs ? '.refs' : ''}${this.#pubkey ? '.enc' : ''}`
+  }
+}
 
 declare var Zotero: { // eslint-disable-line no-var
   platformMajorVersion: number
@@ -46,8 +115,6 @@ type ExportTranslator = {
   setItems: (items: any[]) => void
   translate: () => void
 }
-
-import * as UZip from 'uzip'
 
 class DebugLogSender {
   public id = {
@@ -116,56 +183,24 @@ class DebugLogSender {
     })
   }
 
-  private async sendAsync(plugin: string, preferences: string[], pubkey?: string) {
+  private async sendAsync(plugin: string, preferences: string[], pubkey = '') {
     await Zotero.Schema.schemaUpdatePromise
 
-    const files: Record<string, Uint8Array> = {}
-    const enc = new TextEncoder()
-
-    const key: string = Zotero.Utilities.generateObjectKey()
+    const bundler = new Bundler(pubkey)
 
     let log = [
       await this.info(preferences),
       Zotero.getErrors(true).join('\n\n'),
       Zotero.Debug.getConsoleViewerOutput().slice(-250000).join('\n'), // eslint-disable-line no-magic-numbers
     ].filter((txt: string) => txt).join('\n\n').trim()
-    files[`${key}/debug.txt`] = enc.encode(log)
+    bundler.add('debug.txt', log)
 
     let rdf = await this.rdf()
-    if (rdf) files[`${key}/items.rdf`] = enc.encode(rdf)
+    if (rdf) bundler.add('items.rdf', rdf, true)
 
-    // do this runtime because Zotero is not defined at start for bootstrapped zoter6 plugins
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    if (typeof FormData === 'undefined' && Zotero.platformMajorVersion >= 102) Components.utils.importGlobalProperties(['FormData'])
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    const zip = new Uint8Array(UZip.encode(files) as ArrayBuffer)
-
-    let blob: Blob
-    let ext = ''
-
-    if (pubkey) {
-      try {
-        const subtle = Zotero.getMainWindow().crypto.subtle
-        const pem = pubkey
-          .replace('-----BEGIN PUBLIC KEY-----', '')
-          .replace('-----END PUBLIC KEY-----', '')
-          .replace(/\s/g, '')
-        const keyBuffer = Uint8Array.from(atob(pem), c => c.charCodeAt(0)).buffer
-        const publicKey = await subtle.importKey('spki', keyBuffer, { name: 'RSA-OAEP', hash: 'SHA-256' }, true, ['encrypt'])
-        const encrypted = await subtle.encrypt({ name: 'RSA-OAEP' }, publicKey, zip)
-        blob = new Blob([encrypted], { type: 'application/octet-stream' })
-        ext = '.enc'
-      }
-      catch (err) {
-        Services.prompt.alert(null, `Log encryption for ${plugin} failed`, err.message)
-      }
-    }
-
-    if (!blob) blob = new Blob([zip], { type: 'application/zip' })
-
+    const blob = new Blob([bundler.zip], { type: 'application/zip' })
     const formData = new FormData()
-    formData.append('file', blob, `${key}${ext || '.zip'}`)
+    formData.append('file', blob, bundler.name)
     formData.append('expire', `${7 * 24}`)
 
     try {
@@ -179,7 +214,7 @@ class DebugLogSender {
       const body = await response.text()
       const id = body.match(/https:\/\/0x0.st\/([A-Z0-9]+)\.zip/i)
       if (!id) throw new Error(body)
-      Services.prompt.alert(null, `Debug log ID for ${plugin}`, `${key}-0x0-${id[1]}{ext}`)
+      Services.prompt.alert(null, `Debug log ID for ${plugin}`, bundler.id(`0x0-${id[1]}`))
     }
     catch (err) {
       Services.prompt.alert(null, `Could not post debug log for ${plugin}`, err.message)

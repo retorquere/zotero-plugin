383e5e984e8791b2d80ec59948adb4966c0f078e
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-08-12 22:04:18 +0200

    enc

--- bin/fetch-zotero-log.py
+++ bin/fetch-zotero-log.py
@@ -6,9 +6,6 @@ from zipfile import ZipFile
 from types import SimpleNamespace
 from pathlib import Path
 
-from Cryptodome.PublicKey import RSA
-from Cryptodome.Cipher import PKCS1_OAEP
-
 def oops(*args):
   print(*args)
   sys.exit(1)
@@ -33,24 +30,31 @@ def debuglog():
   references = ('.refs' in tags)
   cypher_rsa = None
   if encrypted:
-    if len(len(sys.argv) != 3):
+    if len(sys.argv) != 3:
       oops('no private key provided')
     if Path(sys.argv[2]).suffix != '.pem':
       oops('private key must be a .pem')
 
-  url = f'https://0x0.com/{remote}.zip'
-  SimpleNamespace(key=key, host=host, remote=remote, encrypted=encrypted, references=references, url=url),
+  url = f'https://0x0.st/{remote}.zip'
+  return SimpleNamespace(key=key, host=host, remote=remote, encrypted=encrypted, references=references, url=url)
 debuglog = debuglog()
+
+if debuglog.encrypted:
+  from cryptography.hazmat.primitives.asymmetric import padding
+  from cryptography.hazmat.primitives import hashes
+  from cryptography.hazmat.primitives.serialization import load_pem_private_key
+  from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+  from cryptography.hazmat.backends import default_backend
 
 def download():
   debuglog.zip = f'logs/{debuglog.key}.zip'
-  print(debuglog.url, '=>', debuglog.zip)
-  logs = os.path.dirname(log)
+  logs = os.path.dirname(debuglog.zip)
   if not os.path.exists(logs):
     os.makedirs(logs)
+  print(debuglog.url, '=>', debuglog.zip)
   urllib.request.urlretrieve(debuglog.url, debuglog.zip)
 
-def decrypt(encrypted, iv, target):
+def decrypt(symmetric_key, encrypted, iv, target):
   encrypted = encrypted.read()
   iv = iv.read()
 
@@ -58,10 +62,11 @@ def decrypt(encrypted, iv, target):
   tag = encrypted[-16:]
   ciphertext = encrypted[:-16]
 
-  cipher_aes = AES.new(symmetric_key, AES.MODE_GCM, nonce=iv)
-  decrypted = cipher_aes.decrypt_and_verify(ciphertext, tag)
+  cipher = Cipher(algorithms.AES(symmetric_key), modes.GCM(iv, tag), backend=default_backend())
+  decryptor = cipher.decryptor()
+
   with open(target, 'wb') as f:
-    f.write(decrypted)
+    f.write(decryptor.update(ciphertext) + decryptor.finalize())
 
 def unpack():
   symmetric_key = None
@@ -70,35 +75,39 @@ def unpack():
     files = zip.namelist()
 
     symmetric_key = [f for f in files if Path(f).suffix == '.key']
-    match len(private_key):
+    match len(symmetric_key):
       case 0:
         if debuglog.encrypted:
           oops('No key found in', debuglog.key)
 
       case 1:
         with open(sys.argv[2], 'rb') as f:
-          private_key = RSA.import_key(f.read())
-          cipher_rsa = PKCS1_OAEP.new(private_key)
+          private_key = load_pem_private_key(f.read(), password=None, backend=default_backend())
         with zip.open(symmetric_key[0]) as f:
-          symmetric_key = cipher_rsa.decrypt(f.read())
+          symmetric_key = private_key.decrypt(f.read(), padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+          ))
 
       case _:
         oops('Multiple keys found in', debuglog.key)
 
     for name in files:
       match Path(name).suffix:
-        case '.key' | '.iv'
+        case '.key' | '.iv':
           pass
-        case '.enc'
-          print(Path('logs') / name.with_suffix('.zip'), '(encrypted)')
-          iv = next((f for f in filename_list if f == Path(name).with_suffix('.iv')), None)
+        case '.enc':
+          print(Path('logs') / Path(name).with_suffix('.zip'), '(encrypted)')
+          iv = next((f for f in files if f == Path(name).with_suffix('.iv')), None)
+          print(name, iv, files)
 
-          with zip.open(name) as f_data, open(iv) as f_iv:
-            decrypt(f_data, f_iv, str(Path('logs') / name))
+          with zip.open(name) as f_data, zip.open(iv) as f_iv:
+            decrypt(symmetric_key, f_data, f_iv, str(Path('logs') / name))
 
         case _:
           print(Path('logs') / name)
           f.extract(name, path='logs')
 
 download()
-decrypt()
+unpack()

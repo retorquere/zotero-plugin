6a8fe04a47d12e6777e46fedd008a3813ad552c9
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-09-09 00:51:08 +0200

    zipup

--- bin/zipup.ts
+++ bin/zipup.ts
@@ -2,7 +2,7 @@
 
 /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
 
-import * as archiver from 'archiver'
+import archiver from 'archiver'
 import * as fs from 'fs'
 import * as path from 'path'
 
@@ -16,7 +16,41 @@ console.log(`creating ${xpi}`) // eslint-disable-line no-console
 if (fs.existsSync(xpi)) fs.unlinkSync(xpi)
 if (!fs.existsSync(path.dirname(xpi))) fs.mkdirSync(path.dirname(xpi))
 
-const archive = archiver.create('zip', {})
-archive.pipe(fs.createWriteStream(xpi))
-archive.directory(`${root}/${source}`, false)
-archive.finalize()
+async function main() {
+  await new Promise<void>((resolve, reject) => {
+    const xpi = path.join(root, 'xpi', `${target}-${version}.xpi`)
+    const output = fs.createWriteStream(xpi)
+    const archive = archiver('zip', { zlib: { level: 9 } })
+
+    output.on('close', () => {
+      console.log(archive.pointer() + ' total bytes')
+      console.log('Archiver has been finalized and the output file descriptor has closed.')
+      resolve()
+    })
+
+    archive.on('warning', err => {
+      if (err.code === 'ENOENT') {
+        console.warn(err.message)
+      }
+      else {
+        console.error(err)
+      }
+    })
+
+    archive.on('error', err => {
+      reject(err)
+    })
+
+    archive.pipe(output)
+
+    // Add files to the archive
+    archive.directory(`${root}/${source}`, false)
+
+    archive.finalize()
+  })
+}
+
+main().catch(err => {
+  console.log(err)
+  process.exit(1)
+})

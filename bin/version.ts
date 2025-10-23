e570f1b6419f8ca8a9e4ee3ad75cee3a9fe1a97f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-10-24 00:01:25 +0200

    version

--- bin/version.ts
+++ bin/version.ts
@@ -10,20 +10,20 @@ import { ContinuousIntegration as CI } from './continuous-integration'
 import { pkg, root } from './find-root'
 
 export function version(): string {
-  let version = pkg.version as string
+  let $version = pkg.version as string
 
   if (CI.service && !CI.tag) {
     const issue = CI.issue && process.env.VERSION_WITH_ISSUE !== 'false' ? `.${CI.issue}` : ''
-    version = `${version}${issue}.${CI.build_number}`
+    $version = `${$version}${issue}.${CI.build_number}`
   }
   else if (!CI.service) {
-    version = `${version}.${os.userInfo().username}.${os.hostname()}`
+    $version = `${$version}.${os.userInfo().username}.${os.hostname()}`
   }
 
   const version_module = path.join(root, 'gen', 'version.cjs')
   if (!fs.existsSync(path.dirname(version_module))) fs.mkdirSync(path.dirname(version_module))
-  fs.writeFileSync(version_module, `module.exports = { version: ${JSON.stringify(version)} }`)
-  return version
+  fs.writeFileSync(version_module, `module.exports = { version: ${JSON.stringify($version)} }`)
+  return $version
 }
 
 version()

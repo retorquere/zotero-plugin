a33e332d5400134d3c7d30ee8b799e92f5c91ba3
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2026-09-09 09:57:55 +0200

    build numbers

---
+++ bin/build.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import os from 'node:os'
+
+import { ContinuousIntegration as CI } from './continuous-integration'
+import { pkg } from './find-root'
+
+export let version = ''
+export let release = Boolean(CI.tag)
+
+export function buildNumber(n = 4) {
+  version = pkg.version as string
+
+  if (!release && CI.service) {
+    version += '.' + `${CI.build_number % (10 ** n)}`.padStart(n, '0') // assuming you are not outputting thousands of builds a week
+  }
+  else if (!release) {
+    version += `.${os.userInfo().username}.${os.hostname()}`
+  }
+}
+
+buildNumber()
